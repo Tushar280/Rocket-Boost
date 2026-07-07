3bc4ce27ab7be383b10d7c4af437232eaf7b62fe
Tushar280 <chaudarytushar280@gmail.com>
2026-07-07 23:17:44 +0530

    Update CollisionHandler.cs

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -32,9 +32,8 @@ public class CollisionHandler : MonoBehaviour
 
     private void LoadNextLevel()
     {
-        int currentIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
         int nextIndex = currentIndex + 1;
-        
         if(currentIndex == SceneManager.sceneCountInBuildSettings - 1)
         {
             nextIndex = 0;
