dddb570d72f7f36c1a8cff28708a51eeca1ccb61
Tushar280 <chaudarytushar280@gmail.com>
2026-07-12 22:44:46 +0530

    Update Oscillator.cs

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,7 +5,19 @@ public class Oscillator : MonoBehaviour
     [SerializeField] Vector3 movementVector;
     [SerializeField] float speed;
 
-    
+    Vector3 startPosition;
+    Vector3 endPosition;
+    float movementFactor;
+
+    void Start()
+    {
+       startPosition = transform.endPosition;
+    }
+
+    void Update()
+    {
+       
+    }
 
     
 }
