585dcee41011624994d503304c253067f27e1ebd
Tushar280 <chaudarytushar280@gmail.com>
2026-07-12 22:43:16 +0530

    Update Oscillator.cs

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -3,12 +3,9 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     [SerializeField] Vector3 movementVector;
-    [SerializeField] float period = 2f;
+    [SerializeField] float speed;
 
-    private void Update()
-    {
-        float cycle01 = Time.time / period; // goes 0 -> 1 and back to 0
-    }
+    
 
     
 }
