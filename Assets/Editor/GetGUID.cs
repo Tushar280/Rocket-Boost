b4078dcb1d235ad4ec75f37aae235fd14181b5bc
Tushar280 <chaudarytushar280@gmail.com>
2026-07-22 23:27:47 +0530

    Changes

--- Assets/Editor/GetGUID.cs
+++
@@ -1,1 +0,0 @@
-using UnityEditor; using UnityEngine; public class GetGUID { [InitializeOnLoadMethod] static void PrintGUID() { System.IO.File.WriteAllText("guid_output.txt", AssetDatabase.AssetPathToGUID("Assets/Scripts/MainMenuManager.cs")); } }
